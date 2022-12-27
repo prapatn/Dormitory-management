119645485fbb37a6448e4aa677781890402e10d2
prapatn <king_44peeraphat@hotmail.com>
2022-12-27 08:53:43 +0700

    edit dorm ui

--- resources/views/owner/dormitories/index.blade.php
+++ resources/views/owner/dormitories/index.blade.php
@@ -18,12 +18,9 @@
             </div>
             @endif
 
-
-
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- Table --}}
                 <div class="col-md-12">
-
                     <div class="card">
                         <div class="card-header text-xl font-semibold">ตารางรายการหอพัก</div>
                         <div class="card-body">
@@ -37,7 +34,48 @@
                                                 {{ __('เพิ่มข้อมูลหอพักใหม่') }}</a>
                                         </div>
                                         @if (count($dormitories)!=0)
-                                        <table class="table table-striped mt-2">
+                                        <div
+                                            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 md:gap-8">
+                                            @foreach ($dormitories as $item)
+                                            <div class="relative">
+                                                <div class="rounded-md shadow-md">
+                                                    <a href="{{ url('dormitories/show/' . $item->id) }}">
+                                                        <img src="{{ $item->image?$item->image:asset('images/buildings.png') }}"
+                                                            class="hover:shadow-lg cursor-pointer rounded-md h-48 w-full object-cover rounded-b-none ">
+                                                    </a>
+                                                    <div class="pt-4 bg-white rounded-b-md p-4">
+                                                        <div class="flex items-start">
+                                                            <div class="flex-1">
+                                                                <h4 class="font-bold leading-6 text-gray-900">
+                                                                    <a href="{{ url('dormitories/show/' . $item->id) }}"
+                                                                        class="text-decoration-none font-bold leading-6 text-gray-900 "
+                                                                        role="text">
+                                                                        {!! $item->name !!}
+                                                                    </a>
+
+                                                                    <a href="{{ url('dormitories/delete/' . $item->id) }}"
+                                                                        onclick="return confirm('ต้องการลบข้อมูลนี้หรือไม่')"
+                                                                        class="float-right sm border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100 transition duration-150 ease-in-out">
+                                                                        <i data-feather="trash-2" class="sm">
+                                                                        </i>
+                                                                    </a>
+                                                                    <a href="{{ url('dormitories/edit/' . $item->id) }}"
+                                                                        class="float-right sm border-2 border-transparent text-gray-600 rounded-full hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100 transition duration-150 ease-in-out">
+                                                                        <i data-feather="edit-2" class="sm">
+                                                                        </i>
+                                                                    </a>
+                                                                </h4>
+                                                            </div>
+                                                        </div>
+                                                        <p class="line-clamp-3 mt-2">
+                                                            {!! $item->getFullAddress() !!}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                        {{-- <table class="table table-striped mt-2">
                                             <thead class="table-dark">
                                                 <tr>
                                                     <th>รูป</th>
@@ -73,12 +111,15 @@
                                                 </tr>
                                                 @endforeach
                                             </tbody>
-                                        </table>
+                                        </table> --}}
                                         {{-- Tab page --}}
-                                        {{ $dormitories->links() }}
+                                        <div class="mt-4">
+                                            {{ $dormitories->links() }}
+                                        </div>
                                         @endif
 
                                     </div>
+
                                 </div>
                             </div>
                         </div>
